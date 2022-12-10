a9f2f4cda241cb0dd21859a47066d1103ae8ef63
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-10 16:18:06 +0100

    Generisanje konstruktora sa komentarima

--- src/main/java/ba/unsa/etf/rpr/Room.java
+++ src/main/java/ba/unsa/etf/rpr/Room.java
@@ -9,5 +9,17 @@ public class Room {
     private String status;
 
 
-
+    /**A constructor that receives attribute values as parameters and initializes them
+     * @param room_id int value that represents the unique key of object
+     * @param room_type int value that represents the number of beds in room
+     * @param price double value for price
+     * @param VIP_services String value that has 2 possible states: YES/NO
+     * @param status String value that has 2 possible states: busy/free */
+    public Room(int room_id, int room_type, double price, String VIP_services, String status) {
+        this.room_id = room_id;
+        this.room_type = room_type;
+        this.price = price;
+        this.VIP_services = VIP_services;
+        this.status = status;
+    }
 }
