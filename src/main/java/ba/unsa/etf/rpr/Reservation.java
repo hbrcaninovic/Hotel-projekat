f763363d5b3be4ab1a5286597c5e60cf4d9448f7
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-10 16:39:22 +0100

    Dodavanje atributa u klasu rezervacija

--- src/main/java/ba/unsa/etf/rpr/Reservation.java
+++ src/main/java/ba/unsa/etf/rpr/Reservation.java
@@ -1,6 +1,17 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Date;
+
 public class Reservation {
+
+    private int reservation_id;
+    private String status;
+    private Date date_of_arrival;
+    private Date departure_date;
+    private int guest_id;
+    private int room_id;
+
+
 
 
 }
