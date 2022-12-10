e2130ff5a90dae9a88dba6b97d5ea9d6c8ac3ffd
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-10 14:40:53 +0100

    Gnerisanje setera i getera sa komentarima

--- src/main/java/ba/unsa/etf/rpr/Employee.java
+++ src/main/java/ba/unsa/etf/rpr/Employee.java
@@ -36,4 +36,121 @@ public class Employee {
         this.job_title = job_title;
         this.salary = salary;
     }
+
+    /**
+     * Getter method for username attribute
+     * @return String value that represents username
+     * */
+    public String getUsername() {
+        return username;
+    }
+
+    /**Setter method for username attribute */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Getter method for password attribute
+     * @return String value that represents password
+     * */
+    public String getPassword() {
+        return password;
+    }
+
+    /**Setter method for password attribute */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * Getter method for first_name attribute
+     * @return String value that represents first_name
+     * */
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    /**Setter method for first_name attribute */
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    /**
+     * Getter method for last_name attribute
+     * @return String value that represents last_name
+     * */
+    public String getLast_name() {
+        return last_name;
+    }
+
+    /**Setter method for last_name attribute */
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    /**
+     * Getter method for address attribute
+     * @return String value that represents address
+     * */
+    public String getAddress() {
+        return address;
+    }
+
+    /**Setter method for address attribute */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * Getter method for email attribute
+     * @return String value that represents email
+     * */
+    public String getEmail() {
+        return email;
+    }
+
+    /**Setter method for email attribute */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Getter method for hire_date attribute
+     * @return String value that represents hire_date
+     * */
+    public Date getHire_date() {
+        return hire_date;
+    }
+
+    /**Setter method for hire_date attribute */
+    public void setHire_date(Date hire_date) {
+        this.hire_date = hire_date;
+    }
+
+    /**
+     * Getter method for job_title attribute
+     * @return String value that represents job_title
+     * */
+    public String getJob_title() {
+        return job_title;
+    }
+
+    /**Setter method for job_title attribute */
+    public void setJob_title(String job_title) {
+        this.job_title = job_title;
+    }
+
+    /**
+     * Getter method for salary attribute
+     * @return String value that represents salary
+     * */
+    public double getSalary() {
+        return salary;
+    }
+
+    /**Setter method for salary attribute */
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
