b82a9900ec68d58b98e50ae6dd8c0c4c57e77db4
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-10 19:42:48 +0100

    Definisanje metode getByDateRange sa opisom.

--- src/main/java/ba/unsa/etf/rpr/ReservationDao.java
+++ src/main/java/ba/unsa/etf/rpr/ReservationDao.java
@@ -1,4 +1,14 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Date;
+import java.util.List;
+
 public interface ReservationDao extends Dao<Reservation>{
+
+    /**
+     * Searches for reservations that have been made in interval between two dates, including submitted dates.
+     * @param date1 is a date of the earliest reservation
+     * @param date2 is a date of the latest reservation
+     * @return List of reservations made from date1 to date2 */
+    List<Reservation> getByDateRange(Date date1, Date date2);
 }
