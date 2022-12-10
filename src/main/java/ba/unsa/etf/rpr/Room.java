1cdfbed936a282e00564e9ddf7f5f1978b12a7b6
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-10 16:10:17 +0100

    Dodavanje atributa klase

--- src/main/java/ba/unsa/etf/rpr/Room.java
+++ src/main/java/ba/unsa/etf/rpr/Room.java
@@ -1,4 +1,13 @@
 package ba.unsa.etf.rpr;
 
 public class Room {
+
+    private int room_id;
+    private int room_type;
+    private double price;
+    private String VIP_services;
+    private String status;
+
+
+
 }
