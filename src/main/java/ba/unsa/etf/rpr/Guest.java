059bf616541f313e3591d1b15c27d8de78ebe3a5
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-10 14:01:30 +0100

    Dodavanje komentara za dokumentovanje setter i getter metoda

--- src/main/java/ba/unsa/etf/rpr/Guest.java
+++ src/main/java/ba/unsa/etf/rpr/Guest.java
@@ -27,51 +27,80 @@ public class Guest {
         this.contact_number = contact_number;
     }
 
-
+    /**
+     * Getter method for ID attribute
+     * @return int value that represents ID
+     * */
     public int getID() {
         return ID;
     }
 
+    /**Setter method for ID attribute */
     public void setID(int ID) {
         this.ID = ID;
     }
 
+    /**
+     * Getter method for first_name attribute
+     * @return String value that represents first_name
+     * */
     public String getFirst_name() {
         return first_name;
     }
 
+    /**Setter method for first_name attribute */
     public void setFirst_name(String first_name) {
         this.first_name = first_name;
     }
 
+    /**
+     * Getter method for last_name attribute
+     * @return String value that represents last_name
+     * */
     public String getLast_name() {
         return last_name;
     }
 
+    /**Setter method for last_name attribute */
     public void setLast_name(String last_name) {
         this.last_name = last_name;
     }
 
+    /**
+     * Getter method for address attribute
+     * @return String value that represents address
+     * */
     public String getAddress() {
         return address;
     }
 
+    /**Setter method for address attribute */
     public void setAddress(String address) {
         this.address = address;
     }
 
+    /**
+     * Getter method for email attribute
+     * @return String value that represents E-mail
+     * */
     public String getEmail() {
         return email;
     }
 
+    /**Setter method for email attribute */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Getter method for Contact_number attribute
+     * @return String value that represents Contact_number
+     * */
     public String getContact_number() {
         return contact_number;
     }
 
+    /**Setter method for Contact_number attribute */
     public void setContact_number(String contact_number) {
         this.contact_number = contact_number;
     }
