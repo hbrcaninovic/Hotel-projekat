de799f49e8b0ab06e303211aae8d348973dba030
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-10 19:09:21 +0100

    Definisanje metoda getAfterByHireDate i getBeforeByHireDate sa opisom.

--- src/main/java/ba/unsa/etf/rpr/EmployeeDao.java
+++ src/main/java/ba/unsa/etf/rpr/EmployeeDao.java
@@ -1,5 +1,22 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Date;
+import java.util.List;
+
 public interface EmployeeDao extends Dao<Employee>{
+
+    /**
+     * Gives all employees which hire date comes after the given (including the forwarded date).
+     * @param date Date type parameter for searching
+     * @return List of employees who were employed after the given date*/
+    List<Employee> getAfterByHireDate(Date date);
+
+    /**
+     * Gives all employees which hire date comes before the given (including the forwarded date).
+     * @param date Date type parameter for searching
+     * @return List of employees who were employed before the given date
+     * */
+    List<Employee> getBeforeByHireDate(Date date);
+
 
 }
