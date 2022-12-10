295f4c073538c13d56faf6f3716633e86b96777c
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-10 18:44:52 +0100

    Definisanje metode getByDateRange sa opisom.

--- src/main/java/ba/unsa/etf/rpr/GuestDao.java
+++ src/main/java/ba/unsa/etf/rpr/GuestDao.java
@@ -1,5 +1,14 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Date;
+import java.util.List;
+
 public interface GuestDao extends Dao<Guest>{
 
+    /**
+     * Searches for guests who have arrived or made a reservation in the interval between two dates, including submitted dates.
+     * @param date1 is a date of arriving (check-in date)
+     * @param date2 check-out date
+     * @return List of guests who stayed in the hotel from date1 to date2 */
+    List<Guest> getByDateRange(Date date1, Date date2);
 }
