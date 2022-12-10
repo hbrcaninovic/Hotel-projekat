a5881eda1a0165e2c812637a374777d2243032df
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-10 13:37:09 +0100

    Dodavanje konsruktora

--- src/main/java/ba/unsa/etf/rpr/Gost.java
+++ src/main/java/ba/unsa/etf/rpr/Gost.java
@@ -9,9 +9,14 @@ public class Gost {
     private String email;
     private String contact_number;
 
-
-
-
+    public Gost(int ID, String first_name, String last_name, String address, String email, String contact_number) {
+        this.ID = ID;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.address = address;
+        this.email = email;
+        this.contact_number = contact_number;
+    }
 
 
 }
