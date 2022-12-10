4da33386990b31d96500e140a2f4ed3e05c77b3c
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-10 13:52:41 +0100

    Dodavanje setter i getter metoda

--- src/main/java/ba/unsa/etf/rpr/Guest.java
+++ src/main/java/ba/unsa/etf/rpr/Guest.java
@@ -28,4 +28,51 @@ public class Guest {
     }
 
 
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContact_number() {
+        return contact_number;
+    }
+
+    public void setContact_number(String contact_number) {
+        this.contact_number = contact_number;
+    }
 }
