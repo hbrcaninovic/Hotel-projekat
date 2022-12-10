dc59d534ae4a9dc00c8b8a417508f803c2c0025e
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-10 19:36:29 +0100

    Definisanje metode searchByStatus sa opisom.

--- src/main/java/ba/unsa/etf/rpr/RoomDao.java
+++ src/main/java/ba/unsa/etf/rpr/RoomDao.java
@@ -1,5 +1,14 @@
 package ba.unsa.etf.rpr;
 
+import java.util.List;
+
 public interface RoomDao extends Dao<Room>{
+
+    /**
+     * Search for all available or occupied rooms.
+     * @param status String which define status (busy/free) for searching rooms
+     * @return List of rooms that satisfies the status condition
+     * */
+    List<Room> searchByStatus(String status);
 
 }
