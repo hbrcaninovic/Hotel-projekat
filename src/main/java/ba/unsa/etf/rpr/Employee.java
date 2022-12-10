b7760e99b51a37a92203083394db6173718770d1
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-10 14:25:57 +0100

    Dodavanje atributa u klasu Employee

--- src/main/java/ba/unsa/etf/rpr/Employee.java
+++ src/main/java/ba/unsa/etf/rpr/Employee.java
@@ -1,4 +1,19 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Date;
+
 public class Employee {
+
+    private String username;
+    private String password;
+    private String first_name;
+    private String last_name;
+    private String address;
+    private String email;
+    private Date hire_date;
+    private String job_title;
+    private double salary;
+
+
+
 }
