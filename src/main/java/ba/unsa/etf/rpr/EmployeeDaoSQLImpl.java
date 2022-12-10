d101e375741f4c537c1973b3e054abc2e07246c8
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-10 19:56:55 +0100

    Kreiranje klase EmployeeDaoSQLImpl i dodavanja metoda koje se nasljeđuju.

---
+++ src/main/java/ba/unsa/etf/rpr/EmployeeDaoSQLImpl.java
@@ -0,0 +1,46 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Date;
+import java.util.List;
+
+public class EmployeeDaoSQLImpl implements EmployeeDao{
+    @Override
+    public Employee getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Employee add(Employee item) {
+        return null;
+    }
+
+    @Override
+    public Employee update(Employee item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Employee> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Employee> getAfterByHireDate(Date date) {
+        return null;
+    }
+
+    @Override
+    public List<Employee> getBeforeByHireDate(Date date) {
+        return null;
+    }
+
+    @Override
+    public List<Employee> getByJob(String job_title) {
+        return null;
+    }
+}
