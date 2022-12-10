2a01d23ba897de285ee6de4c17dfbb9605546e4c
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-10 13:34:21 +0100

    Dodavanje atributa u Gost class

--- src/main/java/ba/unsa/etf/rpr/Gost.java
+++ src/main/java/ba/unsa/etf/rpr/Gost.java
@@ -2,7 +2,16 @@ package ba.unsa.etf.rpr;
 
 public class Gost {
 
-   
+    private int ID;
+    private String first_name;
+    private String last_name;
+    private String address;
+    private String email;
+    private String contact_number;
+
+
+
+
 
 
 }
