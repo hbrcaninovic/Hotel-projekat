631cd7e6a8316e75d78671662e4c33055ce3ce5d
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-11 14:57:06 +0100

    Implementacija privatne metode za nalaženje maksimalnog ID-a u tabeli.

--- src/main/java/ba/unsa/etf/rpr/GuestDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/GuestDaoSQLImpl.java
@@ -1,9 +1,42 @@
 package ba.unsa.etf.rpr;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
 
 public class GuestDaoSQLImpl implements GuestDao {
+
+    private Connection conn;
+    private DB_konekcija k=new DB_konekcija();
+
+
+    /** Private method for finding a maximum _id in database */
+    private int getMaxId(){
+        int id=1;
+        try {
+            PreparedStatement statement = this.conn.prepareStatement("SELECT MAX(zaposlenik_id)+1 FROM zaposlenici");
+            ResultSet rs = statement.executeQuery();
+            if(rs.next()) {
+                id = rs.getInt(1);
+                rs.close();
+                return id;
+            }
+        } catch (SQLException e) {
+            System.out.println("Problem pri traženju maksimalnog indeksa!");
+            System.out.println(e.getMessage());
+        }
+        return id;
+    }
+
+
+
+
+
+
+
     @Override
     public Guest getById(int id) {
         return null;
