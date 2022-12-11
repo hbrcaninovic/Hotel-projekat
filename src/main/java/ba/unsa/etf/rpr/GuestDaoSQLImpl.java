ab0e93c9dfbc4b80f96e3a6b112ac1b4e73a4ffa
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-11 14:51:14 +0100

    Generisanje zaglavlja metoda za implementaciju.

--- src/main/java/ba/unsa/etf/rpr/GuestDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/GuestDaoSQLImpl.java
@@ -1,4 +1,36 @@
 package ba.unsa.etf.rpr;
 
-public class GuestDaoSQLImpl {
+import java.util.Date;
+import java.util.List;
+
+public class GuestDaoSQLImpl implements GuestDao {
+    @Override
+    public Guest getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Guest add(Guest item) {
+        return null;
+    }
+
+    @Override
+    public Guest update(Guest item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Guest> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Guest> getByDateRange(Date date1, Date date2) {
+        return null;
+    }
 }
