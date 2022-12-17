2406fd370e4894bdc47f6817b0eed8df1cb74213
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-18 00:57:17 +0100

    Implementacija start metode

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -1,5 +1,25 @@
 package ba.unsa.etf.rpr;
 
-public class AppFX {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+
+public class AppFX extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
+        primaryStage.setTitle("Hello, World!");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 
 }
