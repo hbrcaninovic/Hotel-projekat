1145ada51fa0efbca5b11e802d31cbffb4a2d279
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-17 20:13:23 +0100

    Kreiranje klase ReservationDaoSQLImpl.

---
+++ src/main/java/ba/unsa/etf/rpr/ReservationDaoSQLImpl.java
@@ -0,0 +1,36 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Date;
+import java.util.List;
+
+public class ReservationDaoSQLImpl implements ReservationDao{
+    @Override
+    public Reservation getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Reservation add(Reservation item) {
+        return null;
+    }
+
+    @Override
+    public Reservation update(Reservation item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Reservation> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Reservation> getByDateRange(Date date1, Date date2) {
+        return null;
+    }
+}
