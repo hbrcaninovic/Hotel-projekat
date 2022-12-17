9d1a4961f4ecb04de30e76c01d58fb09b1e06dd3
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-17 20:26:22 +0100

    Implementacija metode getById.

--- src/main/java/ba/unsa/etf/rpr/ReservationDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/ReservationDaoSQLImpl.java
@@ -1,8 +1,6 @@
 package ba.unsa.etf.rpr;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Date;
 import java.util.List;
 
@@ -26,9 +24,39 @@ public class ReservationDaoSQLImpl implements ReservationDao{
     }
 
 
-
     @Override
     public Reservation getById(int id) {
+        try
+        {
+            PreparedStatement stmt=this.conn.prepareStatement("SELECT * FROM `freedb_RPR baza - projekt`.rezervacije WHERE broj_rezervacije=?");
+            stmt.setInt(1,id);
+            ResultSet rs= stmt.executeQuery();
+
+            if(rs.next())
+            {
+                Reservation r=new Reservation();
+
+                r.setReservation_id(rs.getInt("broj_rezervacije"));
+                r.setStatus(rs.getString("status"));
+                r.setDate_of_arrival(rs.getDate("datum_dolaska"));
+                r.setDeparture_date(rs.getDate("datum_odlaska"));
+                r.setGuest_id(rs.getInt("gost_id"));
+                r.setRoom_id(rs.getInt("br_sobe"));
+
+                rs.close();
+                return r;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Greska prilikom izvrsavanja upita za pristup elementu preko id-a!");
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
