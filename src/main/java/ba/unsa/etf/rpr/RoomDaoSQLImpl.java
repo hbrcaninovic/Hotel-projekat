e13427d5fdaa501415467ffc7bf98512caed7507
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-17 19:57:06 +0100

    Implementacija metode getById.

--- src/main/java/ba/unsa/etf/rpr/RoomDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/RoomDaoSQLImpl.java
@@ -25,6 +25,36 @@ public class RoomDaoSQLImpl implements RoomDao {
 
     @Override
     public Room getById(int id) {
+        try
+        {
+            PreparedStatement stmt=this.conn.prepareStatement("SELECT * FROM `freedb_RPR baza - projekt`.sobe WHERE broj_sobe=?");
+            stmt.setInt(1,id);
+            ResultSet rs= stmt.executeQuery();
+
+            if(rs.next())
+            {
+                Room r=new Room();
+
+                r.setRoom_id(rs.getInt("broj_sobe"));
+                r.setRoom_type(rs.getInt("tip_sobe"));
+                r.setPrice(rs.getDouble("cijena"));
+                r.setVIP_services(rs.getString("VIP"));
+                r.setStatus(rs.getString("status"));
+
+                rs.close();
+                return r;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Greska prilikom izvrsavanja upita za pristup elementu preko id-a!");
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
