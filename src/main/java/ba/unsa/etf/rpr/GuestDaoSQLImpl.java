3768ba5e604be1474157ad543327e16204ea4a78
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-17 18:52:57 +0100

    Implementiranje metoda iz interfaca GuestDao

--- src/main/java/ba/unsa/etf/rpr/GuestDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/GuestDaoSQLImpl.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.rpr;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -14,7 +15,7 @@ public class GuestDaoSQLImpl implements GuestDao {
     private int getMaxId(){
         int id=1;
         try {
-            PreparedStatement statement = this.conn.prepareStatement("SELECT MAX(zaposlenik_id)+1 FROM zaposlenici");
+            PreparedStatement statement = this.conn.prepareStatement("SELECT max(gost_id) FROM `freedb_RPR baza - projekt`.gosti");
             ResultSet rs = statement.executeQuery();
             if(rs.next()) {
                 id = rs.getInt(1);
@@ -42,39 +43,192 @@ public class GuestDaoSQLImpl implements GuestDao {
         }
     }
 
-
-
-
-
-
-
     @Override
     public Guest getById(int id) {
+        try
+        {
+            PreparedStatement stmt=this.conn.prepareStatement("SELECT * FROM `freedb_RPR baza - projekt`.gosti WHERE gost_id=?");
+            stmt.setInt(1,id);
+            ResultSet rs= stmt.executeQuery();
+
+            if(rs.next())
+            {
+                Guest g=new Guest();
+                g.setID(rs.getInt("gost_id"));
+                g.setFirst_name(rs.getString("ime"));
+                g.setLast_name(rs.getString("prezime"));
+                g.setAddress(rs.getString("adresa"));
+                g.setEmail(rs.getString("email"));
+                g.setContact_number(rs.getString("kontakt_broj"));
+
+                rs.close();
+                return g;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Greska prilikom izvrsavanja upita za pristup elementu preko id-a!");
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
+    //???
     @Override
     public Guest add(Guest item) {
+
+        try
+        {
+            PreparedStatement stmt=this.conn.prepareStatement("INSERT INTO `freedb_RPR baza - projekt`.gosti (gost_id, ime, prezime, adresa, email, kontakt_broj) VALUES (?,?,?,?,?,?)");
+            stmt.setInt(1,item.getID());
+            stmt.setString(2,item.getFirst_name());
+            stmt.setString(3,item.getLast_name());
+            stmt.setString(4,item.getAddress());
+            stmt.setString(5,item.getEmail());
+            stmt.setString(6,item.getContact_number());
+
+            stmt.executeUpdate();
+
+            ResultSet rs = stmt.getGeneratedKeys();
+
+            // 98-111
+            if(rs.next())
+            {
+                Guest g=new Guest();
+                g.setID(rs.getInt("gost_id"));
+                g.setFirst_name(rs.getString("ime"));
+                g.setLast_name(rs.getString("prezime"));
+                g.setAddress(rs.getString("adresa"));
+                g.setEmail(rs.getString("email"));
+                g.setContact_number(rs.getString("kontakt_broj"));
+
+                rs.next(); // we know that there is one key
+                item.setID(rs.getInt(1)); //set id to return it back
+                return item;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Greska prilikom izvrsavanja upita za pristup elementu preko id-a!");
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
     @Override
     public Guest update(Guest item) {
+        try
+        {
+            PreparedStatement stmt = this.conn.prepareStatement("UPDATE `freedb_RPR baza - projekt`.gosti SET ime = ?, prezime=?, adresa = ?, email = ?, kontakt_broj =? WHERE gost_id = ?");
+
+            stmt.setString(1,item.getFirst_name());
+            stmt.setString(2,item.getLast_name());
+            stmt.setString(3,item.getAddress());
+            stmt.setString(4,item.getEmail());
+            stmt.setString(5,item.getContact_number());
+            stmt.setInt(6,item.getID());
+
+            stmt.executeUpdate();
+            return item;
+        }
+        catch (SQLException e){
+            System.out.println("Problem pri ažuriranju sloga u bazi podataka");
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
     @Override
     public void delete(int id) {
 
+        try
+        {
+            PreparedStatement stmt =this.conn.prepareStatement("DELETE FROM `freedb_RPR baza - projekt`.gosti WHERE gost_id=?");
+            stmt.setInt(1,id);
+            stmt.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            System.out.println("Greška prilikom brisanja sloga!");
+            System.out.println(e.getMessage());
+        }
+
     }
 
     @Override
     public List<Guest> getAll() {
-        return null;
+
+        List<Guest> guests = new ArrayList<>();
+
+        try
+        {
+            PreparedStatement stmt = this.conn.prepareStatement("SELECT * FROM `freedb_RPR baza - projekt`.gosti");
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()){
+                Guest g = new Guest();
+
+                g.setID(rs.getInt("gost_id"));
+                g.setFirst_name(rs.getString("ime"));
+                g.setLast_name(rs.getString("prezime"));
+                g.setAddress(rs.getString("adresa"));
+                g.setEmail(rs.getString("email"));
+                g.setContact_number(rs.getString("kontakt_broj"));
+                guests.add(g);
+            }
+
+            rs.close();
+        }
+        catch (SQLException e) {
+            System.out.println("Problem pri kopiranju svih slogova tebele u listu!");
+            System.out.println(e.getMessage());
+        }
+        return guests;
     }
 
+    //???
     @Override
     public List<Guest> getByDateRange(Date date1, Date date2) {
-        return null;
+        List<Guest> guests = new ArrayList<>();
+
+        try
+        {
+            PreparedStatement stmt = this.conn.prepareStatement("SELECT * FROM `freedb_RPR baza - projekt`.gosti g, `freedb_RPR baza - projekt`.rezervacije r WHERE  g.gost_id=r.gost_id AND  r.datum_dolaska>=? AND  r.datum_odlaska<=?");
+            stmt.setDate(1, new java.sql.Date (date1.getTime()));
+            stmt.setDate(2, new java.sql.Date (date2.getTime()));
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()){
+                Guest g = new Guest();
+
+                g.setID(rs.getInt("gost_id"));
+                g.setFirst_name(rs.getString("ime"));
+                g.setLast_name(rs.getString("prezime"));
+                g.setAddress(rs.getString("adresa"));
+                g.setEmail(rs.getString("email"));
+                g.setContact_number(rs.getString("kontakt_broj"));
+                guests.add(g);
+
+                System.out.println("---------------------------");
+                for(Guest m:guests)
+                    System.out.println(m.toString());
+            }
+
+            rs.close();
+        }
+        catch (SQLException e) {
+            System.out.println("Problem pri kopiranju svih slogova tebele u listu!");
+            System.out.println(e.getMessage());
+        }
+        return guests;
     }
 }
