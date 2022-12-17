dba75c20974e40da7bc8e1155427829d76891847
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-17 19:01:10 +0100

    Implementacija konstruktora za uspostavljanje konekcije.

--- src/main/java/ba/unsa/etf/rpr/RoomDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/RoomDaoSQLImpl.java
@@ -1,8 +1,28 @@
 package ba.unsa.etf.rpr;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 public class RoomDaoSQLImpl implements RoomDao {
+
+    private Connection conn;
+    private DB_konekcija k=new DB_konekcija();
+
+    /** Constructor that establishes a connection to the database. */
+    public RoomDaoSQLImpl() {
+        try
+        {
+            this.conn= DriverManager.getConnection(k.getUrl(),k.getUser(), k.getPassword());
+        }
+        catch (SQLException e)
+        {
+            System.out.println("Greška pri uspostavljanju konekcije sa bazom podataka!");
+            System.out.println(e.getMessage());
+        }
+    }
+
     @Override
     public Room getById(int id) {
         return null;
