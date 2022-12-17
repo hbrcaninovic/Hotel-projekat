b98d6300a34ea69bf00957edf34538fb2b75301c
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-17 20:17:09 +0100

    Implementacija konstruktora za uspostavljanje konekcije sa bazom podataka.

--- src/main/java/ba/unsa/etf/rpr/ReservationDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/ReservationDaoSQLImpl.java
@@ -1,9 +1,32 @@
 package ba.unsa.etf.rpr;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
 
 public class ReservationDaoSQLImpl implements ReservationDao{
+
+    private Connection conn;
+    private DB_konekcija k=new DB_konekcija();
+
+    /** Constructor that establishes a connection to the database. */
+    public ReservationDaoSQLImpl()
+    {
+        try
+        {
+            this.conn= DriverManager.getConnection(k.getUrl(),k.getUser(), k.getPassword());
+        }
+        catch (SQLException e)
+        {
+            System.out.println("Greška pri uspostavljanju konekcije sa bazom podataka!");
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+
     @Override
     public Reservation getById(int id) {
         return null;
