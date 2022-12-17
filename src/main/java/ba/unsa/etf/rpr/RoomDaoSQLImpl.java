602c5f2738dc595d452bd96dfa93f7b194b2c4cc
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-17 18:58:15 +0100

    Kreiranje klase RoomDaoSQLImpl

---
+++ src/main/java/ba/unsa/etf/rpr/RoomDaoSQLImpl.java
@@ -0,0 +1,35 @@
+package ba.unsa.etf.rpr;
+
+import java.util.List;
+
+public class RoomDaoSQLImpl implements RoomDao {
+    @Override
+    public Room getById(int id) {
+        return null;
+    }
+
+    @Override
+    public Room add(Room item) {
+        return null;
+    }
+
+    @Override
+    public Room update(Room item) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Room> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Room> searchByStatus(String status) {
+        return null;
+    }
+}
