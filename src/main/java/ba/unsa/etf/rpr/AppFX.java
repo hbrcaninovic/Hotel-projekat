9d1a8b079540a94a816769e536ef513f845588e0
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-18 13:32:14 +0100

    Dodavanje logo ikone u zaglavlju prozora.

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import javax.swing.*;
@@ -17,6 +18,7 @@ public class AppFX extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
         primaryStage.setTitle("HOME - Log in");
         primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        primaryStage.getIcons().add(new Image("/img/logo.png"));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
