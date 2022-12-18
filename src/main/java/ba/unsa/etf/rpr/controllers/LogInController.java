09a2330d2356888c2b3f8ccdcb36bd9f00903b2c
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2022-12-18 13:14:30 +0100

    Dodavanje akcije u kontroler.

--- src/main/java/ba/unsa/etf/rpr/controllers/LogInController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LogInController.java
@@ -1,4 +1,9 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+
 public class LogInController {
+    public void akcijaPrijava(ActionEvent actionEvent) {
+        // Validacija
+    }
 }
