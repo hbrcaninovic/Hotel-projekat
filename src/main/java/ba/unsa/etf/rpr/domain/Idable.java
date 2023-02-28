8e81ed0051986f3aa859337f3b4460ee0ddff498
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2023-02-28 15:59:11 +0100

    Kreiranje Idable interface-a

---
+++ src/main/java/ba/unsa/etf/rpr/domain/Idable.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.domain;
+
+/**
+ * Interface Idable contains set and get methods to force any class to implement
+ * this methods, actualy to contain ID element.
+ */
+public interface Idable {
+
+    void setId(int i);
+    int getId();
+}
