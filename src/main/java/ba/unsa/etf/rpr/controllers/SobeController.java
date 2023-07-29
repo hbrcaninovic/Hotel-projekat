4212eb5fa627f995cfb2f5a4590280695cd05d88
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2023-07-29 10:56:51 +0200

    Adding SobeController class file

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/SobeController.java
@@ -0,0 +1,32 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.KeyEvent;
+
+public class SobeController {
+
+    @FXML
+    public void initialize()
+    {
+
+    }
+
+    public SobeController() {
+    }
+
+    public void dodajSobu(ActionEvent actionEvent) {
+    }
+
+    public void azurirajSobu(ActionEvent actionEvent) {
+    }
+
+    public void brisanjeSobe(ActionEvent actionEvent) {
+    }
+
+    public void akcijaNazad(ActionEvent actionEvent) {
+    }
+
+    public void validirajUnosBrojaSobe(KeyEvent keyEvent) {
+    }
+}
