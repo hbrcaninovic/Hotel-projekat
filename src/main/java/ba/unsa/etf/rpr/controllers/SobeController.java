5a8b230b35dd9e79fbf35764f7c2bd452dfc9174
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2023-07-29 14:31:05 +0200

    Implementing controllers methods

--- src/main/java/ba/unsa/etf/rpr/controllers/SobeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SobeController.java
@@ -1,15 +1,43 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.RoomManager;
+import ba.unsa.etf.rpr.domain.Room;
+import ba.unsa.etf.rpr.exceptions.HotelExceptions;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
 
 public class SobeController {
 
-    @FXML
-    public void initialize()
-    {
+    private final RoomManager roomManager = new RoomManager();
 
+    public Button dodajSobuBtn;
+    public Button azurirajSobuBtn;
+    public Button brisanjeSobeBtn;
+    public TextField brojSobeEditText;
+    public Button nazadBtn;
+    public TableView<Room> tabelaSoba;
+    public TableColumn<Room, Integer> brojSobe;
+    public TableColumn<Room, Integer> tipSobe;
+    public TableColumn<Room, String> statusSobe;
+    public TableColumn<Room, String> vipUsluge;
+    public TableColumn<Room, Double> cijena;
+
+
+
+    @FXML
+    public void initialize(){
+        brojSobe.setCellValueFactory(new PropertyValueFactory<Room, Integer>("id"));
+        tipSobe.setCellValueFactory(new PropertyValueFactory<Room, Integer>("room_type"));
+        statusSobe.setCellValueFactory(new PropertyValueFactory<Room, String>("status"));
+        vipUsluge.setCellValueFactory(new PropertyValueFactory<Room, String>("VIP_services"));
+        cijena.setCellValueFactory(new PropertyValueFactory<Room, Double>("price"));
+
+        refreshRoomTable();
     }
 
     public SobeController() {
@@ -25,8 +53,22 @@ public class SobeController {
     }
 
     public void akcijaNazad(ActionEvent actionEvent) {
+        Stage stage = (Stage) nazadBtn.getScene().getWindow();
+        stage.close();
     }
 
     public void validirajUnosBrojaSobe(KeyEvent keyEvent) {
     }
+
+
+    private void refreshRoomTable() {
+        try {
+            tabelaSoba.setItems(FXCollections.observableList(roomManager.getAllRooms()));
+            tabelaSoba.refresh();
+
+        } catch (HotelExceptions e) {
+            new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+        }
+    }
+
 }
