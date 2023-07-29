16aa34d1524fc48b2dfbda079bc3fb3cce87ea8c
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2023-07-29 10:43:02 +0200

    Update main methods

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,7 +1,6 @@
 package ba.unsa.etf.rpr;
 
-import java.util.Date;
-import java.util.List;
+import ba.unsa.etf.rpr.dao.DaoFactory;
 
 /**
  * Hello world!
@@ -9,9 +8,33 @@ import java.util.List;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println("Hello world!");
+    public static void main( String[] args ) {
+      /*  LocalDate d=LocalDate.of(2020,2,15);
+        Employee e=new Employee(11,"test1","t2","TEST","TEST","adress","mail",d,"decorator",1220,0);
+        try {
+            DaoFactory.employeeDao().add(e);
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+
+
+       */
+
+
+
+/*
+            System.out.println("LogIn test");
+            System.out.println(DaoFactory.employeeDao().getAdminStatusByUsernameAndPassword("worker1","w123"));
+            System.out.println(DaoFactory.employeeDao().getAdminStatusByUsernameAndPassword("admin","a123"));
+        System.out.println(DaoFactory.employeeDao().getAdminStatusByUsernameAndPassword("adn","a123"));
+
+
+ */
+
+
+
 
 
 
--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -7,24 +7,33 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import javax.swing.*;
-
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class AppFX extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
-        primaryStage.setTitle("HOME - Log in");
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        primaryStage.setTitle("HOME - Log in"); // Postavlja tekstualno zaglavlje prozora
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE)); // Kreira Scenu prema USE_COMPUTED_SIZE konstanti
+        primaryStage.getIcons().add(new Image("/img/logo.png")); //Dodavanje ikone u zaglavlju prozora
+        primaryStage.setResizable(false); //Onemugućavanje izmjene veličine prozora
+        primaryStage.show();  // Poziv za prikaz prozora
+
+
+
+/*
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/zaposlenici.fxml"));
+        primaryStage.setTitle("HOME - Recepcija");
         primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        //primaryStage.setMinWidth(1000);//Minimalna širina
+        //primaryStage.setMinHeight(500); //Min visina prozora
         primaryStage.getIcons().add(new Image("/img/logo.png"));
-        primaryStage.setResizable(false);
-        primaryStage.show();
+        //primaryStage.setResizable(false);
+        primaryStage.show();*/
     }
     public static void main(String[] args) {
         launch(args);
     }
-
 
 }
