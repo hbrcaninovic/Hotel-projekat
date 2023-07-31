e8b3b696fd3af56bd7179d18bc7ac1cd526a2b1d
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2023-07-31 23:31:04 +0200

    Implementing RecepcijaController

--- src/main/java/ba/unsa/etf/rpr/controllers/RecepcijaController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RecepcijaController.java
@@ -1,14 +1,27 @@
 package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.domain.Employee;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class RecepcijaController {
 
-    public static Employee employee;
+    public static Employee employee = new Employee();
     public Label welcomeLabel;
+    public Button odjavaBtn;
+    public Button checkInBtn;
 
     public RecepcijaController(Employee employee) {
         this.employee = employee;
@@ -16,7 +29,32 @@ public class RecepcijaController {
 
     @FXML
     public void initialize(){
-        welcomeLabel.setText(employee.getFirst_name());
-        System.out.println(employee.getFirst_name());
+        welcomeLabel.setText("Dobrodošli ".concat(employee.getFirst_name()));
+    }
+
+    public void akcijaOdjava(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) odjavaBtn.getScene().getWindow();
+        stage.close();
+
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        stage.setTitle("HOME - Log in"); // Postavlja tekstualno zaglavlje prozora
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE)); // Kreira Scenu prema USE_COMPUTED_SIZE konstanti
+        stage.getIcons().add(new Image("/img/logo.png")); //Dodavanje ikone u zaglavlju prozora
+        stage.setResizable(false); //Onemugućavanje izmjene veličine prozora
+        stage.show();  // Poziv za prikaz prozora
+    }
+
+    public void akcijaCheckIn(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/checkIn.fxml"));
+        stage.setTitle("HOME - Check in"); // Postavlja tekstualno zaglavlje prozora
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE)); // Kreira Scenu prema USE_COMPUTED_SIZE konstanti
+        stage.getIcons().add(new Image("/img/logo.png")); //Dodavanje ikone u zaglavlju prozora
+        stage.setResizable(false); //Onemugućavanje izmjene veličine prozora
+        stage.show();  // Poziv za prikaz prozora
+
+
+
     }
 }
