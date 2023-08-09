6939259ac7fe9dedcb4a8fb6818c74a49c3a64a9
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2023-08-09 23:25:44 +0200

    Implementing CheckOut action

--- src/main/java/ba/unsa/etf/rpr/controllers/CheckOutController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/CheckOutController.java
@@ -1,21 +1,23 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.GuestManager;
 import ba.unsa.etf.rpr.business.ReservationManager;
+import ba.unsa.etf.rpr.business.RoomManager;
+import ba.unsa.etf.rpr.domain.Guest;
 import ba.unsa.etf.rpr.domain.Reservation;
+import ba.unsa.etf.rpr.domain.Room;
 import ba.unsa.etf.rpr.exceptions.HotelExceptions;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.time.Duration;
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Optional;
 
 public class CheckOutController {
     public ChoiceBox<Reservation> brojSobeBox;
@@ -32,22 +34,45 @@ public class CheckOutController {
 
 
     ReservationManager reservationManager = new ReservationManager();
+    GuestManager guestManager = new GuestManager();
+    RoomManager roomManager = new RoomManager();
     List<Reservation> reservationList = null;
+    Room room = new Room();
+    Guest guest = new Guest();
 
     public CheckOutController() {
-
 
     }
 
     @FXML
     public void initialize() throws HotelExceptions {
         reservationList = reservationManager.getAllReservations();
+
         brojSobeBox.getItems().addAll(reservationList);
+
         brojSobeBox.setOnAction(e->{
+            if (brojSobeBox.getValue() != null) {
+                try {
+
+                    room = roomManager.getRoom(brojSobeBox.getValue().getRoom_id());
+                    guest = guestManager.getGuest(brojSobeBox.getValue().getGuest_id());
+                }
+                catch (HotelExceptions ex)
+                {
+                    System.out.println(ex);
+                }
+            }
 
             datumDolaskaDatePicker.setValue(brojSobeBox.getValue().getDate_of_arrival());
             datumOdlaskaDatePicker.setValue(brojSobeBox.getValue().getDeparture_date());
-            iznosTextField.setText(String.valueOf(ChronoUnit.DAYS.between(datumDolaskaDatePicker.getValue(),datumOdlaskaDatePicker.getValue())));
+
+            jmbgTextField.setText(String.valueOf(guest.getId()));
+            imeGostaTextField.setText(guest.getFirst_name());
+            prezimeGostaTextField.setText(guest.getLast_name());
+            mailGostaTextField.setText(guest.getEmail());
+            kontaktBrojGostaTextField.setText(guest.getContact_number());
+
+            iznosTextField.setText(String.valueOf((ChronoUnit.DAYS.between(datumDolaskaDatePicker.getValue(),datumOdlaskaDatePicker.getValue())*room.getPrice())));
 
         });
 
@@ -61,5 +86,18 @@ public class CheckOutController {
 
 
     public void akcijaCheckOut(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Potvrda odjave");
+        alert.setContentText("Da li ste sigurni da želite odjaviti goste s ove rezervacije?");
+        Optional<ButtonType> response = alert.showAndWait();
+
+        if (response.get() == ButtonType.OK) {
+            room.setStatus("slobodna");
+
+            roomManager.updateRoom(room);
+            reservationManager.deleteReservation(brojSobeBox.getValue());
+            guestManager.deleteGuest(guest);
+
+        }
     }
 }
