51e138fbbc73651ac757e6db25396d0986c455c9
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2023-08-29 19:08:40 +0200

    Refactoring - adding UtilityMethodsForWindows class to extract same code

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/UtilityMethodsForWindows.java
@@ -0,0 +1,56 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
+public class UtilityMethodsForWindows {
+    public UtilityMethodsForWindows() {
+    }
+
+    public void openWindow(String fxmlStagePath, String title) throws IOException {
+
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource(fxmlStagePath));
+        stage.setTitle("HOME - ".concat(title)); // Postavlja tekstualno zaglavlje prozora
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE)); // Kreira Scenu prema USE_COMPUTED_SIZE konstanti
+        stage.getIcons().add(new Image("/img/logo.png")); //Dodavanje ikone u zaglavlju prozora
+        stage.setResizable(false); //Onemugućavanje izmjene veličine prozora
+        stage.show();  // Poziv za prikaz prozora
+
+    }
+
+    public static void openErrorAlertWindow(String title, String headerText, String contentText) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.setContentText(contentText);
+        alert.showAndWait();
+    }
+
+    public static void openInformationAlertWindow(String title, String headerText, String contentText) {
+        Alert information = new Alert(Alert.AlertType.INFORMATION);
+        information.setTitle(title);
+        information.setHeaderText(headerText);
+        information.setContentText(contentText);
+        information.show();
+    }
+
+    public static Optional<ButtonType> openConfirmationAlertWindow(String title, String headerText, String contentText) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.setContentText(contentText);
+        Optional<ButtonType> response = alert.showAndWait();
+        return response;
+    }
+}
