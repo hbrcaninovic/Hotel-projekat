80ff365db5583553a4347942a99f4b3356b64319
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2023-09-08 10:35:39 +0200

    Addding java doc comments

--- src/main/java/ba/unsa/etf/rpr/controllers/IUserLogIn.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/IUserLogIn.java
@@ -5,5 +5,6 @@ import ba.unsa.etf.rpr.domain.Employee;
 import java.io.IOException;
 
 public interface IUserLogIn {
+    /** The method opens a window for the user whose access is granted by authentication */
     void openUserDasboard() throws IOException;
 }
