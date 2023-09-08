8bfcc1c9aaeba597611341f24d13cb95de8d7c4c
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2023-09-08 07:56:14 +0200

    Addding java doc comments

--- src/main/java/ba/unsa/etf/rpr/exceptions/HotelExceptions.java
+++ src/main/java/ba/unsa/etf/rpr/exceptions/HotelExceptions.java
@@ -1,11 +1,19 @@
 package ba.unsa.etf.rpr.exceptions;
 
-public class HotelExceptions extends Exception{
+/** Class that represents user defined Exception */
+public class HotelExceptions extends Exception {
 
+    /** HotelsException with two parameters
+     * @param msg message
+     * @param exp exception from Exception class
+     */
     public HotelExceptions(String msg, Exception exp){
         super(msg,exp);
     }
 
+    /** HotelsException with one parameter
+     * @param msg message
+     */
     public HotelExceptions(String msg){
         super(msg);
     }
