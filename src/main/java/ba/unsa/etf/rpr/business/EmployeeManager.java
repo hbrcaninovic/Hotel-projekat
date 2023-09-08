f17d6e00c3ef9afb55da50a63c9490a9c84abd44
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2023-09-08 09:04:38 +0200

    Addding java doc comments

--- src/main/java/ba/unsa/etf/rpr/business/EmployeeManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/EmployeeManager.java
@@ -9,9 +9,18 @@ import java.util.List;
 
 public class EmployeeManager {
 
+    /** Gives information about the employee from the database based on the username
+     * @param username Stirng value that represents employee's username
+     * @return Employee value that username is same as given
+     * */
     public Employee getEmployee(String username) throws HotelExceptions {
         return DaoFactory.employeeDao().getByUsername(username);
     }
+
+    /** Adds a employee to the database
+     * @param employee mployee object that represent employee
+     * @return boolean value which gives confirmation of the successful addition of the employee to the database
+     * */
     public boolean addEmployee(Employee employee) throws HotelExceptions {
         try {
             DaoFactory.employeeDao().add(employee);
@@ -25,6 +34,10 @@ public class EmployeeManager {
         }
     }
 
+    /** Updates the specified employee from the database
+     * @param employee Employee object that updates
+     * @return boolean value which gives confirmation of the successful update of the employee to the database
+     * */
     public boolean updateEmployee(Employee employee) {
         try {
             DaoFactory.employeeDao().update(employee);
@@ -35,10 +48,17 @@ public class EmployeeManager {
         }
     }
 
+    /** A method that returns a list of all employees from the database
+     * @return List of Employee
+     * */
     public List<Employee> getAllEmployees() throws HotelExceptions {
         return DaoFactory.employeeDao().getAll();
     }
 
+    /** Deletes the specified employee from the database
+     * @param employee Employee object that deletes
+     * @return boolean value which gives confirmation of the successful deletion of the employee to the database
+     * */
     public boolean deleteEmployee(Employee employee){
         try {
             DaoFactory.employeeDao().deleteEmployee(employee.getId());
