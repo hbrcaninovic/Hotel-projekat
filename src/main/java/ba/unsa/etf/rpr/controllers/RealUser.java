ae8f9378d22105cc1c8bd7f4c37a2b5d99489b0a
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2023-09-08 10:35:51 +0200

    Addding java doc comments

--- src/main/java/ba/unsa/etf/rpr/controllers/RealUser.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RealUser.java
@@ -6,10 +6,13 @@ import java.io.IOException;
 
 public class RealUser implements IUserLogIn{
     Employee employee;
+
+    /** Constructor */
     public RealUser(Employee employee) {
         this.employee = employee;
     }
 
+    /** The method opens a window for the user whose access is granted by authentication */
     @Override
     public void openUserDasboard() throws IOException {
         if (employee.getAdmin() == 0) UtilityMethodsForWindows.openWindow2("/fxml/recepcija.fxml",
