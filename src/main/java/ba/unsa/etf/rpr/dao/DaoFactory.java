46eac1b3e128ee45489e5a7d17dc9b4bb839fdda
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2023-09-08 08:36:12 +0200

    Addding java doc comments

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -7,22 +7,27 @@ public class DaoFactory {
     private static final ReservationDao reservationDao = ReservationDaoSQLImpl.getInstance();
     private static final RoomDao roomDao = RoomDaoSQLImpl.getInstance();
 
+    /** Implicit constructor */
     public DaoFactory() {
 
     }
 
+    /** Gets an instance EmployeeDao */
     public static EmployeeDao employeeDao(){
         return employeeDao;
     }
 
+    /** Gets an instance GuestDao */
     public static GuestDao guestDao(){
         return guestDao;
     }
 
+    /** Gets an instance ReservationDao */
     public static ReservationDao reservationDao(){
         return  reservationDao;
     }
 
+    /** Gets an instance RoomDao */
     public static RoomDao roomDao(){
         return roomDao;
     }
